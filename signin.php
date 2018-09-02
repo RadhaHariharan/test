0cf0cb27810d1fc16060cf402f05d5f981903c39
RadhaHariharan <42894756+RadhaHariharan@users.noreply.github.com>
2018-09-02 16:49:14 +0530

    Add files via upload

---
+++ signin.php
@@ -0,0 +1,21 @@
+<?php
+
+	require_once 'login.php';
+	$conn = new mysqli($hn,$un,$pw,$db);
+
+	if ($_SERVER['REQUEST_METHOD']=='GET') {
+		$username = $_GET['username'];
+		$password = $_GET['password'];
+		$query = "SELECT * FROM signin WHERE username = '$username' AND password = '$password'";
+		$result = $conn->query($query);
+		$check = mysqli_fetch_array($result);
+		if(isset($check)) {
+			echo 'success';
+			echo $check;
+		} else {
+			echo 'failure';
+		}
+
+	}
+
+?>
